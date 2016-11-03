00b66aba605b0f3bc05ac35104fdfb9b31de1de1
Maciej Burchardt <burgundt3@gmail.com>
2016-11-03 12:16:52 +0100

    GameObject class add

---
+++ ClickerGameEngine/ClickerGameEngine/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ClickerGameEngine
+{
+    class GameObject
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Production { get;  set; }
+        public Bitmap bitmap { get; set; }
+
+        public int ProductionGet()
+        {
+            int value = Production;
+
+            for (int i = 0; i < Level; i++)
+            {
+                value += ((Production * Level) / 10);
+            }
+
+            return value;
+        }
+    }
+}
