bfa64c5fbecd22a73b9e089f60fe9a4aff502646
Maciej Burchardt <burgundt3@gmail.com>
2016-11-03 20:20:36 +0100

    add comments in GameObject.cs

--- ClickerGameEngine/ClickerGameEngine/GameObject.cs
+++ ClickerGameEngine/ClickerGameEngine/GameObject.cs
@@ -5,25 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
+/// <summary>
+/// This is our main game object. Increasing numbers/levels of game object will increase player earnings
+/// </summary>
 namespace ClickerGameEngine
 {
     class GameObject
     {
+        //Name of game object, f.e.: "house" if you want city-clicker or "new vlog" if you want youtuber-clicker etc.
         public string Name { get; set; }
+
+        //Level of game object (you can also understood it as amount of this kind game objects)
         public int Level { get; set; }
+        //Production earnings, f.e. in city-clicker "Office Tower" should be more profitable than "Kiosk"
         public int Production { get;  set; }
-        public Bitmap bitmap { get; set; }
+        //increasing number/level of each game object should require an investment from player
+        public int Price { get; set; }
 
-        public int ProductionGet()
-        {
-            int value = Production;
-
-            for (int i = 0; i < Level; i++)
-            {
-                value += ((Production * Level) / 10);
-            }
-
-            return value;
-        }
+        //100x100 pixels bitmap of this game object
+        public Bitmap Bitmap { get; set; }
     }
 }
