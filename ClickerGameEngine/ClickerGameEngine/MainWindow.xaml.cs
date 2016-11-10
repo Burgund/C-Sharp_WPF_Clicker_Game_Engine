f9d3e73dffae2b76c3eb2e295bdadd9a4d4268c8
Maciej Burchardt <burgundt3@gmail.com>
2016-11-10 14:04:08 +0100

    add MoneyPerSecondUpdate function

--- ClickerGameEngine/ClickerGameEngine/MainWindow.xaml.cs
+++ ClickerGameEngine/ClickerGameEngine/MainWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace ClickerGameEngine
         private int _moneyPerSec = 0;
         //Each click earnings
         private int _moneyPerClick = 1;
+        //Set up game objects 
+        private GameObject[] _gameObjectArray = new GameObject[8];
 
         public MainWindow()
         {
@@ -44,59 +46,58 @@ namespace ClickerGameEngine
             //Timer start
             _timer.Start();
 
-            //Set up game objects
-            GameObject[] gameObjectArray = new GameObject[8];
-            gameObjectArray = GameSetUp.BuildGameObjects();
+            //Set up game objects  
+            _gameObjectArray = GameSetUp.BuildGameObjects();
 
             //Set up game object bitmap to UI
-            GameObjectImage1.Source = gameObjectArray[0].GetBitmap();
-            GameObjectImage2.Source = gameObjectArray[1].GetBitmap();
-            GameObjectImage3.Source = gameObjectArray[2].GetBitmap();
-            GameObjectImage4.Source = gameObjectArray[3].GetBitmap();
-            GameObjectImage5.Source = gameObjectArray[4].GetBitmap();
-            GameObjectImage6.Source = gameObjectArray[5].GetBitmap();
-            GameObjectImage7.Source = gameObjectArray[6].GetBitmap();
-            GameObjectImage8.Source = gameObjectArray[7].GetBitmap();
+            GameObjectImage1.Source = _gameObjectArray[0].GetBitmap();
+            GameObjectImage2.Source = _gameObjectArray[1].GetBitmap();
+            GameObjectImage3.Source = _gameObjectArray[2].GetBitmap();
+            GameObjectImage4.Source = _gameObjectArray[3].GetBitmap();
+            GameObjectImage5.Source = _gameObjectArray[4].GetBitmap();
+            GameObjectImage6.Source = _gameObjectArray[5].GetBitmap();
+            GameObjectImage7.Source = _gameObjectArray[6].GetBitmap();
+            GameObjectImage8.Source = _gameObjectArray[7].GetBitmap();
 
             //Set up game object production to UI
-            ProductionGameObject1.Text = gameObjectArray[0].GetProduction().ToString();
-            ProductionGameObject2.Text = gameObjectArray[1].GetProduction().ToString();
-            ProductionGameObject3.Text = gameObjectArray[2].GetProduction().ToString();
-            ProductionGameObject4.Text = gameObjectArray[3].GetProduction().ToString();
-            ProductionGameObject5.Text = gameObjectArray[4].GetProduction().ToString();
-            ProductionGameObject6.Text = gameObjectArray[5].GetProduction().ToString();
-            ProductionGameObject7.Text = gameObjectArray[6].GetProduction().ToString();
-            ProductionGameObject8.Text = gameObjectArray[7].GetProduction().ToString();
+            ProductionGameObject1.Text = _gameObjectArray[0].GetProduction().ToString();
+            ProductionGameObject2.Text = _gameObjectArray[1].GetProduction().ToString();
+            ProductionGameObject3.Text = _gameObjectArray[2].GetProduction().ToString();
+            ProductionGameObject4.Text = _gameObjectArray[3].GetProduction().ToString();
+            ProductionGameObject5.Text = _gameObjectArray[4].GetProduction().ToString();
+            ProductionGameObject6.Text = _gameObjectArray[5].GetProduction().ToString();
+            ProductionGameObject7.Text = _gameObjectArray[6].GetProduction().ToString();
+            ProductionGameObject8.Text = _gameObjectArray[7].GetProduction().ToString();
 
             //Set up game object price to UI
-            PriceGameObject1.Text = gameObjectArray[0].GetPrice().ToString();
-            PriceGameObject2.Text = gameObjectArray[1].GetPrice().ToString();
-            PriceGameObject3.Text = gameObjectArray[2].GetPrice().ToString();
-            PriceGameObject4.Text = gameObjectArray[3].GetPrice().ToString();
-            PriceGameObject5.Text = gameObjectArray[4].GetPrice().ToString();
-            PriceGameObject6.Text = gameObjectArray[5].GetPrice().ToString();
-            PriceGameObject7.Text = gameObjectArray[6].GetPrice().ToString();
-            PriceGameObject8.Text = gameObjectArray[7].GetPrice().ToString();
+            PriceGameObject1.Text = _gameObjectArray[0].GetPrice().ToString();
+            PriceGameObject2.Text = _gameObjectArray[1].GetPrice().ToString();
+            PriceGameObject3.Text = _gameObjectArray[2].GetPrice().ToString();
+            PriceGameObject4.Text = _gameObjectArray[3].GetPrice().ToString();
+            PriceGameObject5.Text = _gameObjectArray[4].GetPrice().ToString();
+            PriceGameObject6.Text = _gameObjectArray[5].GetPrice().ToString();
+            PriceGameObject7.Text = _gameObjectArray[6].GetPrice().ToString();
+            PriceGameObject8.Text = _gameObjectArray[7].GetPrice().ToString();
 
             //Set up game object level
-            LevelGameObject1.Text = gameObjectArray[0].GetLevel().ToString();
-            LevelGameObject2.Text = gameObjectArray[1].GetLevel().ToString();
-            LevelGameObject3.Text = gameObjectArray[2].GetLevel().ToString();
-            LevelGameObject4.Text = gameObjectArray[3].GetLevel().ToString();
-            LevelGameObject5.Text = gameObjectArray[4].GetLevel().ToString();
-            LevelGameObject6.Text = gameObjectArray[5].GetLevel().ToString();
-            LevelGameObject7.Text = gameObjectArray[6].GetLevel().ToString();
-            LevelGameObject8.Text = gameObjectArray[7].GetLevel().ToString();
+            LevelGameObject1.Text = _gameObjectArray[0].GetLevel().ToString();
+            LevelGameObject2.Text = _gameObjectArray[1].GetLevel().ToString();
+            LevelGameObject3.Text = _gameObjectArray[2].GetLevel().ToString();
+            LevelGameObject4.Text = _gameObjectArray[3].GetLevel().ToString();
+            LevelGameObject5.Text = _gameObjectArray[4].GetLevel().ToString();
+            LevelGameObject6.Text = _gameObjectArray[5].GetLevel().ToString();
+            LevelGameObject7.Text = _gameObjectArray[6].GetLevel().ToString();
+            LevelGameObject8.Text = _gameObjectArray[7].GetLevel().ToString();
 
             //Set up game object name
-            NameGameObject1.Text = gameObjectArray[0].GetName().ToString();
-            NameGameObject2.Text = gameObjectArray[1].GetName().ToString();
-            NameGameObject3.Text = gameObjectArray[2].GetName().ToString();
-            NameGameObject4.Text = gameObjectArray[3].GetName().ToString();
-            NameGameObject5.Text = gameObjectArray[4].GetName().ToString();
-            NameGameObject6.Text = gameObjectArray[5].GetName().ToString();
-            NameGameObject7.Text = gameObjectArray[6].GetName().ToString();
-            NameGameObject8.Text = gameObjectArray[7].GetName().ToString();
+            NameGameObject1.Text = _gameObjectArray[0].GetName().ToString();
+            NameGameObject2.Text = _gameObjectArray[1].GetName().ToString();
+            NameGameObject3.Text = _gameObjectArray[2].GetName().ToString();
+            NameGameObject4.Text = _gameObjectArray[3].GetName().ToString();
+            NameGameObject5.Text = _gameObjectArray[4].GetName().ToString();
+            NameGameObject6.Text = _gameObjectArray[5].GetName().ToString();
+            NameGameObject7.Text = _gameObjectArray[6].GetName().ToString();
+            NameGameObject8.Text = _gameObjectArray[7].GetName().ToString();
         }
 
         //Timer event method
@@ -107,17 +108,28 @@ namespace ClickerGameEngine
 
             //Each second UI will be update 
             moneyTextBox.Text = _money.ToString();
-            moneyPerSecondTextBox.Text = _moneyPerSec.ToString();
             moneyPerClickTextBlock.Text = _moneyPerClick.ToString();
         }
 
-        private void ClickAreaHandler (object sender, MouseEventArgs e)
+        private void ClickAreaHandler(object sender, MouseEventArgs e)
         {
             //increase wallet
             _money += _moneyPerClick;
 
             //UI wallet update
             moneyTextBox.Text = _money.ToString();
+        }
+
+        private void MoneyPerSecondUpdate()
+        {
+            _moneyPerSec = 0;
+
+            for(int i = 0; i < _gameObjectArray.Length; i++)
+            {
+                _moneyPerSec += _gameObjectArray[i].GetProduction();
+            }
+
+            moneyPerSecondTextBox.Text = _moneyPerSec.ToString();
         }
     }
 }
