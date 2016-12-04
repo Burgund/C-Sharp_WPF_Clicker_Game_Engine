336c918555131e930afed516e1f1ae845613ab66
Maciej Burchardt <burgundt3@gmail.com>
2016-12-04 10:19:02 +0100

    code refactoring

--- ClickerGameEngine/ClickerGameEngine/MainWindow.xaml.cs
+++ ClickerGameEngine/ClickerGameEngine/MainWindow.xaml.cs
@@ -22,102 +22,94 @@ namespace ClickerGameEngine
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer _timer;
-        //Player wallet
-        private long _money = 0;
-        //Each secong earnings  
-        private int _moneyPerSec = 0;
-        //Each click earnings
+        private long _playerWallet = 0;
+        private int _moneyPerSecond = 0;
         private int _moneyPerClick = 1;
-        //Set up game objects 
-        private GameObject[] _gameObjectArray = GameSetUp.BuildGameObjects();
+        private readonly GameObject[] _gameObjectAggregator = GameSetUp.BuildGameObjects();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            //Timer object to handle each second auto-click. Game main-loop
-            _timer = new DispatcherTimer(DispatcherPriority.SystemIdle);
-            //Add event method to our timer
-            _timer.Tick += new EventHandler(OnUpdateTimerTick);
-            //Set timer tact to 1sec
-            _timer.Interval = TimeSpan.FromMilliseconds(1000);
-            //Timer start
-            _timer.Start();
+            //Using DispatcherTimer as a game loop
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.SystemIdle);
+            timer.Tick += new EventHandler(OnUpdateTimerTick);
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Start();
 
             //Set up the UI
-            GameSetUp.SetUpUI(_gameObjectArray);
+            GameSetUp.SetUpUI(_gameObjectAggregator);
         }
 
         //Timer event method
         private void OnUpdateTimerTick(object sender, EventArgs e)
         {
             //Player will earn each second
-            _money += _moneyPerSec;
+            _playerWallet += _moneyPerSecond;
 
             //Each second UI will be update 
-            moneyTextBox.Text = _money.ToString();
+            moneyTextBox.Text = _playerWallet.ToString();
             moneyPerClickTextBlock.Text = _moneyPerClick.ToString();
         }
 
         //Each time player will increase level of any object we will have to update MPS value
         private void MoneyPerSecondUpdate()
         {
-            _moneyPerSec = 0;
+            _moneyPerSecond = 0;
 
-            for(int i = 0; i < _gameObjectArray.Length; i++)
+            for(int i = 0; i < _gameObjectAggregator.Length; i++)
             {
-                _moneyPerSec += _gameObjectArray[i].GetProduction();
+                _moneyPerSecond += _gameObjectAggregator[i].GetProduction();
             }
 
-            moneyPerSecondTextBox.Text = _moneyPerSec.ToString();
+            moneyPerSecondTextBox.Text = _moneyPerSecond.ToString();
         }
 
         private void ImageOneClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[0]);
+            BuyNewObject(_gameObjectAggregator[0]);
         }
 
         private void ImageTwoClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[1]);
+            BuyNewObject(_gameObjectAggregator[1]);
         }
 
         private void ImageThreeClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[2]);
+            BuyNewObject(_gameObjectAggregator[2]);
         }
 
         private void ImageFourClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[3]);
+            BuyNewObject(_gameObjectAggregator[3]);
         }
 
         private void ImageFiveClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[4]);
+            BuyNewObject(_gameObjectAggregator[4]);
         }
 
         private void ImageSixClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[5]);
+            BuyNewObject(_gameObjectAggregator[5]);
         }
 
         private void ImageSevenClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[6]);
+            BuyNewObject(_gameObjectAggregator[6]);
         }
 
         private void ImageEightClicked(object sender, MouseEventArgs e)
         {
-            BuyNewObject(_gameObjectArray[7]);
+            BuyNewObject(_gameObjectAggregator[7]);
         }
 
         private void BuyNewObject(GameObject gameObject)
         {
-            if (_money >= gameObject.GetPrice())
+            if (_playerWallet >= gameObject.GetPrice())
             {
-                _money -= gameObject.GetPrice();
+                _playerWallet -= gameObject.GetPrice();
                 gameObject.IncreaseLevel();
                 MoneyPerSecondUpdate();
                 _moneyPerClick += (gameObject.GetProduction() / 8);
@@ -127,16 +119,16 @@ namespace ClickerGameEngine
                 MessageBox.Show("You do not have enough money!");
             }
 
-            GameSetUp.SetUpUI(_gameObjectArray);
+            GameSetUp.SetUpUI(_gameObjectAggregator);
         }
 
         private void ClickAreaHandler(object sender, MouseEventArgs e)
         {
             //increase wallet
-            _money += _moneyPerClick;
+            _playerWallet += _moneyPerClick;
 
             //UI wallet update
-            moneyTextBox.Text = _money.ToString();
+            moneyTextBox.Text = _playerWallet.ToString();
         }
     }
 }
